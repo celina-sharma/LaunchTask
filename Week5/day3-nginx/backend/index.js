7311490c08b84d94d34d2fea2919b5040899c8db
celinas-hestabit <celinas.hestabit@gmail.com>
2026-01-12 16:50:45 +0530

    NGINX reverse proxy, load balancing finally done

--- Week5/day3-nginx/backend/index.js
+++ Week5/day3-nginx/backend/index.js
@@ -2,13 +2,12 @@ import express from "express";
 
 const app = express();
 
-app.get("/",(req,res) => {
-    res.json({
-        messgae:"Response from backend",
-        container: process.env.HOSTNAME
-    });
+app.get("/api", (req, res) => {
+  res.json({
+    messgae: "Hello from backend",
+    container: process.env.HOSTNAME,
+  });
 });
-app.listen(3000,() => {
-    console.log("Backend running on port 3000");
-    
+app.listen(3000, () => {
+  console.log("Backend running on port 3000");
 });
